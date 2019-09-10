8be63d5d352a7f385d6517c920d9f7ce0b608869
Raj Bhatta <bhattaraj21@gmail.com>
2019-09-10 00:59:17 -0600

    How to make a connection from sqlplus using service name
    How to make a connection from sqlplus using service name

---
+++ connection.sql
@@ -0,0 +1,5 @@
+--1. goto command prompt
+--2. sqlplus username/password@host:port/service
+
+---======= Example =================
+sqlplus rajbhatta/rajbhattapwd@app.raj.test.net:1442/springapp.oracle.test
