fd0e419079f5677aa8b14869a95cb3e38e5d60fe
Raj Bhatta <bhattaraj21@gmail.com>
2019-09-12 10:42:15 -0600

    Update connection.sql

--- How to make a connection from sqlplus using service name in oracle/connection.sql
+++ How to make a connection from sqlplus using service name in oracle/connection.sql
@@ -3,3 +3,8 @@
 
 ---======= Example =================
 sqlplus rajbhatta/rajbhattapwd@app.raj.test.net:1442/springapp.oracle.test
+
+
+
+--- ====Setting TNS_ADMIN during session level ========
+set tns_admin=c:\temp 
