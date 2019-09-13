12b4f79caeed4867e20c9235bd09aa67bd675dfc
Raj Bhatta <bhattaraj21@gmail.com>
2019-09-12 23:42:04 -0600

    Update connection.sql

--- How to make a connection from sqlplus using service name in oracle/connection.sql
+++ How to make a connection from sqlplus using service name in oracle/connection.sql
@@ -8,3 +8,7 @@ sqlplus rajbhatta/rajbhattapwd@app.raj.test.net:1442/springapp.oracle.test
 
 --- ====Setting TNS_ADMIN during session level ========
 set tns_admin=c:\temp 
+
+
+--- ========== Useful links =========================
+https://stackoverflow.com/questions/37711482/how-to-install-oracle-instant-client-on-a-mac
